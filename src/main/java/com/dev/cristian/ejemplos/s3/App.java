20f2ac4e8034226f22ea4fcf2d614e0ec5fd5782
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2019-08-26 10:41:09 -0500

    Add S3 SDK to project

---
+++ src/main/java/com/dev/cristian/ejemplos/s3/App.java
@@ -0,0 +1,11 @@
+package com.dev.cristian.ejemplos.s3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/dev/cristian/ejemplos/s3/AppTest.java
@@ -0,0 +1,20 @@
+package com.dev.cristian.ejemplos.s3;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
